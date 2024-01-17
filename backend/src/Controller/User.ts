aef8b0e3945dcee2ebf2c26cdc259b7dd71d55b2
William Correa <wilcorrea@gmail.com>
2024-01-16 21:23:11 -0300

    fix: corrigir resolução de conflito

--- backend/src/Controller/User.ts
+++ backend/src/Controller/User.ts
@@ -1,19 +1,6 @@
 import { Request, Response } from 'express'
 import { connection } from '../../config/database'
 
-<<<<<<< HEAD
-export async function FindUser (request: Request, response: Response) {
-    const { username } = request.query
-    const db = connection()
-    const users = await db
-        .select('id','username')
-        .from('users')
-        .where('username','like',`${username}%`)
-        response
-        .status(200)
-        .json({ status:'success', data:users})
-}
-=======
 export async function FindUser(request: Request, response: Response) {
     const { username } = request.query
     const db = connection()
@@ -25,4 +12,3 @@ export async function FindUser(request: Request, response: Response) {
         .status(200)
         .json({ status: 'success', data: users })
 }
->>>>>>> f2a2021d75c5908b5ac6436441a141467152addd
