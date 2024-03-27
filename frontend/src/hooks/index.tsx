0882e29ecdd9dfaed89fe39b2e7ec52692083271
CarlosFernadesdfs <carlosfernandesdfs@hotmail.com>
2024-03-26 22:00:04 -0300

    fix:corrigir bug-07

--- frontend/src/hooks/index.tsx
+++ frontend/src/hooks/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { AppContext } from '../components/App/AppContext.tsx'
 
-export useApp () {
+export function useApp () {
   return React.useContext(AppContext)
 }
