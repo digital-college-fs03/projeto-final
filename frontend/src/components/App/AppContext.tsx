4f057a1936ece9300a677511147849e4c7f14719
William Correa <wilcorrea@gmail.com>
2024-03-26 21:58:20 -0300

    fix: corrigir problema que o William inventou

--- frontend/src/components/App/AppContext.tsx
+++ frontend/src/components/App/AppContext.tsx
@@ -1,4 +1,4 @@
 import { createContext } from 'react'
 import { AppContextContract } from '../../types/app.ts'
 
-export const App = createContext<AppContextContract>(null!)
+export const AppContext = createContext<AppContextContract>(null!)
